20e00e18eec2f398dae931a612f230adc8ed5229
dohalou99 <dohaelouakili99@gmail.com>
2023-10-15 16:11:58 -0700

    team project

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#<doha> and <youssef>
+""" Class State """
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """[State class]
+    inherits from BaseModel
+    """
+    name = ""
