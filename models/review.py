c33d7f2885e76b8f86e486fa27c7dc2d75073d79
dohalou99 <126737814+dohalou99@users.noreply.github.com>
2023-10-17 23:49:22 +0100

    Add files via upload

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+review class
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
